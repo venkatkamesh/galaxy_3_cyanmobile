36b96d8d7c3285e28a84ebd0aabcc40a242906ac
nani <venkat.kamesh09@gmail.com>
2012-10-08 02:58:01 -0700

    mass commit

---
+++ Android.mk
@@ -0,0 +1,2 @@
+#include $(call all-named-subdir-makefiles, libcamera libaudio libgps libsensors libcopybit libgralloc libsgl liboverlay libstagefrighthw bdaddr_read gadget_id )
+include $(all-subdir-makefiles)
